dda1d85d4016e24753751bff955dfaf37f3d9df3
yassine bouali <yassine.bouali.bo@gmail.com>
2026-09-03 15:43:27 +0100

    fix: change the project from chari baas to api-docs

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename="/chari_baas_docs">
+    <BrowserRouter basename="/api-docs-template">
         <App />
     </BrowserRouter>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    // For gh-pages branch deployment – this makes assets load from /chari_baas_docs/assets/...
-    base: '/chari_baas_docs/', 
+    // For gh-pages branch deployment – this makes assets load from /api-docs-template/assets/...
+    base: '/api-docs-template/', 
     plugins: [react(), tailwindcss()],
 });
